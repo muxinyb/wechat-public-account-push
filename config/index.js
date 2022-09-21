714fa1150cd698e3826f067a1fca26c757478f82
muxinyb <114041271+muxinyb@users.noreply.github.com>
2022-09-21 13:17:05 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ export const config = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "887f31e8cb33a453ce03df2c019f5867",
+  APP_SECRET: "545c5f3390bfdd180515e5d6c5b15877",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "omLZI68cdZuDmiKqoT8b1gRLdS1Q",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "aB36-uyXtp2l_R_WrspoC8X7br1zNN4KgaioDpm9JKA",
+      useTemplateId: "sAvhzreX5URWVhXSKPxVqMCnv-225ovgmmQLGm3KcjE",
       // 所在省份
       province: "贵州",
       // 所在城市
@@ -94,18 +94,7 @@ export const config = {
         {"keyword": "ex_day", date: "2022-09-10"},
       ]
     },
-    {
-      name: "小秋",
-      id: "omLZI68cdZuDmiKqoT8b1gRLdS1Q",
-      useTemplateId: "aB36-uyXtp2l_R_WrspoC8X7br1zNN4KgaioDpm9JKA",
-      province: "贵州",
-      city: "贵阳",
-      horoscopeDate: '09-09',
-      horoscopeDateType: '今日',
-      openUrl: "https://wangxinleo.cn",
-      festivals: [ {"type": "*生日", "name": "小秋", "year": "2022", "date": "09-09"},],
-      customizedDateList: [{"keyword": "love_day", date: "2022-09-08"},]
-    },
+    
     {
       name: "老婆2",
       id: "",
@@ -139,7 +128,7 @@ export const config = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "aB36-uyXtp2l_R_WrspoC8X7br1zNN4KgaioDpm9JKA",
+  CALLBACK_TEMPLATE_ID: "sAvhzreX5URWVhXSKPxVqMCnv-225ovgmmQLGm3KcjE",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
